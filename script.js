0e35caf1c7db3260b702846226ffa0787c197f40
Vatsal Solanki <solankivatsal131@gmail.com>
2024-08-26 18:26:35 +0530

    transition

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.name-header');
+let logoSpan = document.querySelectorAll('.name');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+
+        logoSpan.forEach((span, idx)=>{
+        setTimeout(()=>{
+            span.classList.add('active');
+        }, (idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+           logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, ( idx + 1) * 50)
+        })
+    },2000);
+
+    setTimeout(()=>{
+        intro.style.top ='-100vh';
+    }, 2300)
+})
+
+})
+
+
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
 
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-
 }
